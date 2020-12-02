b3bf324039c72914e55e1fd5f5b388638bcd70eb
nuzulzen <nuzulzen@gmail.com>
2020-12-02 22:31:50 +0700

    fix cart legth and add path cartItem

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Button, Container, Grid, Typography } from "@material-ui/core";
 
 import useStyles from "./styles";
+import CartItem from "./CartItem/CartItem";
 
 function Cart({ cart }) {
   console.log("cart", cart);
 
-  const isEmpty = !cart.line_items.length;
   const classes = useStyles();
 
   const EmptyCart = () => (
@@ -20,7 +20,7 @@ function Cart({ cart }) {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xm={12} sm={4} key={item.id}>
-            <div>{item.name}</div>
+            <CartItem item={item} />
           </Grid>
         ))}
       </Grid>
@@ -52,6 +52,8 @@ function Cart({ cart }) {
     </>
   );
 
+  if (!cart.line_items) return "loading...";
+
   return (
     <Container>
       <div className={classes.toolbar}>
@@ -60,7 +62,7 @@ function Cart({ cart }) {
           Your Shoping Cart
         </Typography>
       </div>
-      {isEmpty ? <EmptyCart /> : <FilledCart />}
+      {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
     </Container>
   );
 }
