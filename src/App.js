4eb56aa812d40095d76a2bf1636bdbf7cb303d38
nuzulzen <nuzulzen@gmail.com>
2020-12-04 15:28:04 +0700

    fix cart bug

--- src/App.js
+++ src/App.js
@@ -14,27 +14,26 @@ function App() {
   };
 
   const fetchCart = async () => {
-    const cart = await commerce.cart.retrieve();
-    setCart(cart);
+    setCart(await commerce.cart.retrieve());
   };
 
   const handleAddToCart = async (productId, quantity) => {
-    const cart = await commerce.cart.add(productId, quantity);
+    const { cart } = await commerce.cart.add(productId, quantity);
     setCart(cart);
   };
 
   const handleUpdateCartQty = async (productId, quantity) => {
-    const cart = await commerce.cart.update(productId, { quantity });
+    const { cart } = await commerce.cart.update(productId, { quantity });
     setCart(cart);
   };
 
   const handleRemoveFromCart = async (productId) => {
-    const cart = await commerce.cart.remove(productId);
+    const { cart } = await commerce.cart.remove(productId);
     setCart(cart);
   };
 
   const handleEmptyCart = async () => {
-    const cart = await commerce.cart.empty();
+    const { cart } = await commerce.cart.empty();
     setCart(cart);
   };
 
